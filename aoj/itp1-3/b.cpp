f124d058505f227a083bf78e314f290e5de24771
yutachaan <yutachan010907@gmail.com>
2021-07-13 21:12:07 +0900

    :sparkles: add itp1-3

---
+++ aoj/itp1-3/b.cpp
@@ -0,0 +1,18 @@
+// AC
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int x, count = 0;
+  while (true) {
+    cin >> x;
+
+    if (x == 0) break;
+
+    count++;
+    cout << "Case " << count << ": " << x << endl;
+  }
+
+  return 0;
+}
