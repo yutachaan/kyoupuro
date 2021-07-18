c93c6da8ae039d217e6269b33df817f114498988
yutachaan <yutachan010907@gmail.com>
2021-07-18 16:45:03 +0900

    :sparkles: add practice

---
+++ other/practiceA.cpp
@@ -0,0 +1,15 @@
+// AC
+// 練習
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int a, b, c;
+  string s;
+  cin >> a >> b >> c >> s;
+
+  cout << a + b + c << ' ' << s << endl;
+
+  return 0;
+}
