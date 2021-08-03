203112e40408af1dc72e4c9651619c7fbfce2664
yutachaan <yutachan010907@gmail.com>
2021-08-03 16:56:18 +0900

    :sparkles: add abc57 c

--- abc-100/057/c.cpp
+++ abc-100/057/c.cpp
@@ -1,8 +1,25 @@
+// AC
+// N=AxBを満たすA, Bの組
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+  long long int N;
+  cin >> N;
+
+  int ans = 100;
+  // 約数列挙の流れでやればよい
+  for (long long int A = 1; A * A <= N; A++) {
+    if (N % A == 0) {
+      // 明らかにAのほうが桁数が小さいため，Bについてのみ考える
+      int B = N / A;
+      int len_B = to_string(B).size();
+
+      if (len_B < ans) ans = len_B;
+    }
+  }
+  cout << ans << endl;
 
   return 0;
 }
