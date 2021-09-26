056ab88eb2a36ead9f3d22f9575dd7ef2454d5f1
yutachaan <yutachan010907@gmail.com>
2021-09-26 22:55:02 +0900

    :sparkles: add abc220 a-c

---
+++ ABC201-250/220/b.cpp
@@ -0,0 +1,20 @@
+// AC
+// 進数変換
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int K;
+  string A, B;
+  cin >> K >> A >> B;
+
+  long long int d_A = 0, d_B = 0;
+  for (int i = A.size() - 1; i >= 0; i--) d_A += (A[i] - '0') * pow(K, A.size() - 1 - i);
+  for (int i = B.size() - 1; i >= 0; i--) d_B += (B[i] - '0') * pow(K, B.size() - 1 - i);
+
+  long long int ans = d_A * d_B;
+  printf("%lld\n", ans);
+
+  return 0;
+}
---
+++ ABC201-250/220/c.cpp
@@ -0,0 +1,33 @@
+// AC
+// 数列Aを連結した数列Bにおいて，Bの項を前から足していったときに初めてXを超える項
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  int A[N];
+  long long int sum = 0;
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+    sum += A[i];
+  }
+
+  long long int X;
+  cin >> X;
+
+  long long int ans = (X / sum) * N + 1;
+  X %= sum;
+  for (int i = 0; i < N; i++) {
+    if (X - A[i] < 0) break;
+
+    X -= A[i];
+    ans++;
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
