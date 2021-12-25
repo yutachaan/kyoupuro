2a183fbedc3ca4da47651c7d9fa403ca52516c8b
yutachaan <yutachan010907@gmail.com>
2021-12-25 12:51:00 +0900

    :sparkles: abs1-3

--- abc-100/081/b.cpp
+++ abc-100/081/b.cpp
@@ -1,28 +1,64 @@
-// AC
-// 「偶数なら2で割る」という操作を何回繰り返せるか
 #include <bits/stdc++.h>
-
 using namespace std;
 
+// <----- alias ----->
+using ll = long long;
+
+const int inf = INT_MAX / 2;
+const ll infl = 1LL << 60;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vs  = vector<string>;
+using pii = pair<int, int>;
+using mii = map<int, int>;
+using msi = map<string, int>;
+
+// <----- REPマクロ ----->
+#define FOR(i, a, b)  for (ll i = (a); i < (ll)(b); i++)
+#define RFOR(i, a, b) for (ll i = (a) - 1; i >= (b); i--)
+#define REP(i, n)     FOR(i, 0, n)
+#define REPS(i, n)    FOR(i, 1, n + 1)
+#define RREP(i, n)    RFOR(i, n, 0)
+#define RREPS(i, n)   RFOR(i, n + 1, 1)
+#define FOREACH(e, x) for (auto&& (e): x)
+
+// <----- 略記 ----->
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <class T> bool chmin(T& a, const T& b) {if (a > b) a = b; return a > b;}
+template <class T> bool chmax(T& a, const T& b) {if (a < b) a = b; return a < b;}
+
+// <----- other ----->
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+
 int main() {
-  int N;
-  cin >> N;
+  int n;
+  cin >> n;
 
-  int A[N], count = 0;
-  for (int i = 0; i < N; i++) cin >> A[i];
+  vi a(n);
+  REP(i, n) cin >> a[i];
 
-  while (true) {
-    int i;
-    for (i = 0; i < N; i++) {
-      if (A[i] % 2 == 0) A[i] /= 2;
-      else break;
+  int ans = 0;
+  bool ok = true;
+  while (ok) {
+    REP(i, n) {
+      if (a[i] % 2) ok = false;
+      else a[i] /= 2;
     }
 
-    if (i == N) count++;
-    else break;
+    if (ok) ans++;
   }
 
-  cout << count << endl;
+  cout << ans << endl;
 
   return 0;
 }
