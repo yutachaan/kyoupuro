2b1d3dd407a8e7afc8cba8351b45f7aab4a1b0d3
yutachaan <yutachan010907@gmail.com>
2021-12-28 12:25:11 +0900

    :sparkles: ass abs1-3 ruidai

---
+++ abc/088/a.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = INT_MAX / 2;
+const ll infl = 1LL << 60;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vs  = vector<string>;
+using pii = pair<int, int>;
+using mii = map<int, int>;
+using msi = map<string, int>;
+
+// <----- REPマクロ ----->
+#define FOR(i, a, b)  for (ll i = (a); i < (ll)(b); i++)
+#define RFOR(i, a, b) for (ll i = (a) - 1; i >= (b); i--)
+#define REP(i, n)     FOR(i, 0, n)
+#define REPS(i, n)    FOR(i, 1, n + 1)
+#define RREP(i, n)    RFOR(i, n, 0)
+#define RREPS(i, n)   RFOR(i, n + 1, 1)
+#define FOREACH(e, x) for (auto&& (e): x)
+
+// <----- 略記 ----->
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <class T> bool chmin(T& a, const T& b) {if (a > b) a = b; return a > b;}
+template <class T> bool chmax(T& a, const T& b) {if (a < b) a = b; return a < b;}
+
+// <----- other ----->
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+
+int main() {
+  int n, a;
+  cin >> n >> a;
+
+  YesNo(n % 500 <= a);
+
+  return 0;
+}
---
+++ abc/095/b.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = INT_MAX / 2;
+const ll infl = 1LL << 60;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vs  = vector<string>;
+using pii = pair<int, int>;
+using mii = map<int, int>;
+using msi = map<string, int>;
+using si  = set<int>;
+using ss  = set<string>;
+
+// <----- REPマクロ ----->
+#define FOR(i, a, b)  for (ll i = (a); i < (ll)(b); i++)
+#define RFOR(i, a, b) for (ll i = (a) - 1; i >= (b); i--)
+#define REP(i, n)     FOR(i, 0, n)
+#define REPS(i, n)    FOR(i, 1, n + 1)
+#define RREP(i, n)    RFOR(i, n, 0)
+#define RREPS(i, n)   RFOR(i, n + 1, 1)
+#define FOREACH(e, x) for (auto&& (e): x)
+
+// <----- 略記 ----->
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+// <----- other ----->
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+
+int main() {
+  int n, x;
+  cin >> n >> x;
+
+  int good_donut_moto = inf;
+  REP(i, n) {
+    int m;
+    cin >> m;
+
+    x -= m;
+    chmin(good_donut_moto, m);
+  }
+
+  int ans = n + x / good_donut_moto; // それぞれ1つずつ + 一番消費量が少ないドーナツで残りを作る
+
+  cout << ans << endl;
+
+  return 0;
+}
