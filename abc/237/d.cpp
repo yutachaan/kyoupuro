8b1a28e2a8c5bc1bfb17e43cba41ad145dc49d35
yutachaan <yutachan010907@gmail.com>
2022-01-31 11:01:46 +0900

    :sparkles: add abc237 a-d

---
+++ abc/237/d.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- debug ----->
+#ifdef LOCAL
+#  include <debug_print.hpp>
+#  define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#  define debug(...) (static_cast<void>(0))
+#endif
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = numeric_limits<int>::max() / 2;
+const ll infl = numeric_limits<ll>::max()  / 2;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vl  = vector<ll>;
+using vvl = vector<vl>;
+using vs  = vector<string>;
+using pii = pair<int, int>;
+using mii = map<int, int>;
+using msi = map<string, int>;
+using si  = set<int>;
+using ss  = set<string>;
+
+#define endl "\n";
+
+// <----- REPマクロ ----->
+#define rep(i, a, b)  for (ll i = (a); i < (ll)(b); i++)
+#define rrep(i, a, b) for (ll i = (a) - 1; i >= (b); i--)
+#define foreach(e, x) for (auto&& (e): x)
+
+// <----- 略記 ----->
+#define ALL(x) begin((x)), end((x))
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << "\n"
+#define yesno(n) cout << ((n) ? "yes" : "no") << "\n"
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << "\n"
+
+// <----- function ----->
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+
+int main() {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  deque<int> q = {n};
+  rrep(i, n, 0) {
+    if (s[i] == 'L') q.push_back(i);
+    else q.push_front(i);
+  }
+
+  foreach(e, q) cout << e << ' ';
+  cout << endl;
+
+  return 0;
+}
