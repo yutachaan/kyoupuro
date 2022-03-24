36049622a7d4a11bdafd6c40569baf170bf9a101
yutachaan <yutachan010907@gmail.com>
2022-03-24 15:28:14 +0900

    :sparkles: add abc223 a-c

---
+++ atcoder/abc/201-250/223/c.cpp
@@ -0,0 +1,67 @@
+// x
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = numeric_limits<int>::max() / 2;
+const ll infl = numeric_limits<ll>::max()  / 2;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vs  = vector<string>;
+using pii = pair<int, int>;
+
+#define endl "\n";
+
+// <----- rep macro ----->
+#define rep(i, a, b)  for (ll i = (a); i < (ll)(b); i++)
+#define rrep(i, a, b) for (ll i = (a) - 1; i >= (b); i--)
+#define fore(e, x) for (auto &&(e): x)
+#define fore2(k, v, x) for (auto &&[k, v]: x)
+
+// <----- other macro ----->
+#define ALL(x) begin((x)), end((x))
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << "\n"
+#define yesno(n) cout << ((n) ? "yes" : "no") << "\n"
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << "\n"
+
+// <----- function ----->
+template <class T>
+inline bool chmax(T &a, T b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+
+int main() {
+  int N; cin >> N;
+  vector<double> A(N), B(N);
+  rep(i, 0, N) cin >> A[i] >> B[i];
+
+  // ぶつかる時間
+  double t = 0;
+  rep(i, 0, N) t += A[i] / B[i];
+  t /= 2;
+
+  // 時刻tのときの位置
+  double ans = 0;
+  rep(i, 0, N) {
+    ans += min(A[i], t * B[i]);
+    t -= A[i] / B[i];
+
+    if (t <= 0) break;
+  }
+
+  printf("%.15lf\n", ans);
+
+  return 0;
+}
