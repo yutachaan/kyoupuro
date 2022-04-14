c451e91875bfe399142b22dfb26a6e1ca71daa72
yutachaan <yutachan010907@gmail.com>
2022-04-14 15:46:58 +0900

    :sparkles: テンプレーつを更新

--- template.cpp
+++ template.cpp
@@ -32,13 +32,13 @@ using pii = pair<int, int>;
 #define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
 
 // <----- function ----->
-template <class T>
-inline bool chmax(T &a, T b) {
+template <typename T>
+bool chmax(T &a, T b) {
   if (a < b) {a = b; return true;}
   return false;
 }
-template <class T>
-inline bool chmin(T &a, T b) {
+template <typename T>
+bool chmin(T &a, T b) {
   if (a > b) {a = b; return true;}
   return false;
 }
