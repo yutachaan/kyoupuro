1f5c1ac5a5183a67a6c64ebb0083f385f271c095
yutachaan <yuuuta010907@gmail.com>
2023-02-12 21:36:19 +0900

    :sparkles: add ma071-080

---
+++ atcoder/other contest/math-and-algorithm/080.cpp
@@ -0,0 +1,149 @@
+// #define _GLIBCXX_DEBUG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = numeric_limits<int>::max() / 2;
+const ll infl = numeric_limits<ll>::max()  / 2;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vl  = vector<ll>;
+using vvl = vector<vl>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvb = vector<vb>;
+using pii = pair<int, int>;
+
+// <----- rep macro ----->
+#define rep(var, a, b)  for (auto var = (a); var < (b); var++)
+#define rrep(var, a, b) for (auto var = a - 1; var >= (b); var--)
+#define fore(e, x) for (auto &&e: x)
+#define fore2(k, v, x) for (auto &[k, v]: x)
+
+// <----- other macro ----->
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <typename T>
+bool chmax(T &a, T b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <typename T>
+bool chmin(T &a, T b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+// <----- ModInt ----->
+template<int MOD> struct modint {
+  ll val;
+  constexpr modint(ll v = 0) noexcept : val(v % MOD) { if (val < 0) val += MOD; }
+  constexpr int getmod() { return MOD; }
+  constexpr modint operator - () const noexcept { return val ? MOD - val : 0; }
+  constexpr modint operator + (const modint& r) const noexcept { return modint(*this) += r; }
+  constexpr modint operator - (const modint& r) const noexcept { return modint(*this) -= r; }
+  constexpr modint operator * (const modint& r) const noexcept { return modint(*this) *= r; }
+  constexpr modint operator / (const modint& r) const noexcept { return modint(*this) /= r; }
+  constexpr modint& operator += (const modint& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr modint& operator -= (const modint& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr modint& operator *= (const modint& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr modint& operator /= (const modint& r) noexcept {
+    ll a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const modint& r) const noexcept { return this->val == r.val; }
+  constexpr bool operator != (const modint& r) const noexcept { return this->val != r.val; }
+  friend constexpr ostream& operator << (ostream &os, const modint<MOD>& x) noexcept { return os << x.val; }
+  constexpr modint<MOD> modpow(ll n) noexcept {
+      if (n == 0) return 1;
+      auto t = modpow(n / 2);
+      t *= t;
+      if (n & 1) t *= modint(*this);
+      return t;
+  }
+};
+
+const int MOD = 1000000007;
+using mint = modint<MOD>;
+
+
+struct Edge {
+  int to;
+  int weight;
+  Edge(int t, int w): to(t), weight(w) { }
+};
+
+vb used;
+vl dist;
+vector<vector<Edge>> G;
+
+// v始点のダイクストラ法
+void dijkstra(int v) {
+  priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> Q;
+
+  dist[v] = 0;
+  Q.push(make_pair(0, v));
+
+  while (!Q.empty()) {
+    int pos = Q.top().second; Q.pop();
+
+    if (used[pos]) continue;
+    used[pos] = true;
+
+    fore2(to, w, G[pos]) {
+      if (pos != v) w += dist[pos];
+
+      if (dist[to] > w) {
+        dist[to] = w;
+        Q.push(make_pair(dist[to], to));
+      }
+    }
+  }
+}
+
+int main() {
+  int N, M; cin >> N >> M;
+  G.assign(N, vector<Edge>());
+  rep(i, 0, M) {
+    int a, b, c; cin >> a >> b >> c;
+    a--; b--;
+    G[a].push_back(Edge(b, c));
+    G[b].push_back(Edge(a, c));
+  }
+
+  dist.assign(N, infl);
+  used.assign(N, false);
+  dijkstra(0);
+
+  if (dist[N - 1] == infl) cout << -1 << endl;
+  else cout << dist[N - 1] << endl;
+}
