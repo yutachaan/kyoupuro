8350fdbb0dcc11abc61463b868a4a082d36f32ee
yutachaan <yutachan010907@gmail.com>
2023-05-13 19:08:39 +0900

    feat: :sparkles: add abc300 a-d

---
+++ atcoder/abc/251-300/300/c.cpp
@@ -0,0 +1,130 @@
+// #define _GLIBCXX_DEBUG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = numeric_limits<int>::max() / 2;
+const ll infl = numeric_limits<ll>::max()  / 2;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vl  = vector<ll>;
+using vvl = vector<vl>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvb = vector<vb>;
+using pii = pair<int, int>;
+
+// <----- rep macro ----->
+#define rep(var, a, b)  for (auto var = (a); var < (b); var++)
+#define rrep(var, a, b) for (auto var = a - 1; var >= (b); var--)
+#define fore(e, x) for (auto &&e: x)
+#define fore2(k, v, x) for (auto &[k, v]: x)
+
+// <----- other macro ----->
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <typename T>
+bool chmax(T &a, T b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <typename T>
+bool chmin(T &a, T b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+// <----- ModInt ----->
+template<int MOD> struct modint {
+  ll val;
+  constexpr modint(ll v = 0) noexcept : val(v % MOD) { if (val < 0) val += MOD; }
+  constexpr int getmod() { return MOD; }
+  constexpr modint operator - () const noexcept { return val ? MOD - val : 0; }
+  constexpr modint operator + (const modint& r) const noexcept { return modint(*this) += r; }
+  constexpr modint operator - (const modint& r) const noexcept { return modint(*this) -= r; }
+  constexpr modint operator * (const modint& r) const noexcept { return modint(*this) *= r; }
+  constexpr modint operator / (const modint& r) const noexcept { return modint(*this) /= r; }
+  constexpr modint& operator += (const modint& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr modint& operator -= (const modint& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr modint& operator *= (const modint& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr modint& operator /= (const modint& r) noexcept {
+    ll a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const modint& r) const noexcept { return this->val == r.val; }
+  constexpr bool operator != (const modint& r) const noexcept { return this->val != r.val; }
+  friend constexpr ostream& operator << (ostream &os, const modint<MOD>& x) noexcept { return os << x.val; }
+  constexpr modint<MOD> modpow(ll n) noexcept {
+      if (n == 0) return 1;
+      auto t = modpow(n / 2);
+      t *= t;
+      if (n & 1) t *= modint(*this);
+      return t;
+  }
+};
+
+const int MOD = 1000000007;
+using mint = modint<MOD>;
+
+
+int main() {
+  int H, W; cin >> H >> W;
+  vs C(H); rep(i, 0, H) cin >> C[i];
+
+  rep(n, 1, min(H, W) + 1) {
+    int ans = 0;
+    rep(a, 0, H) rep(b, 0, W) {
+      if (C[a][b] == '#') {
+        bool ok = true;
+        if ((a + n + 1 < H && b + n + 1 < W && C[a + n + 1][b + n + 1] == '#')
+        && (a + n + 1 < H && b - n - 1 >= 0 && C[a + n + 1][b - n - 1] == '#')
+        && (a - n - 1 >= 0 && b + n + 1 < W && C[a - n - 1][b + n + 1] == '#')
+        && (a - n - 1 >= 0 && b - n - 1 >= 0 && C[a - n - 1][b - n - 1] == '#')) ok = false;
+
+        rep(d, 1, n + 1) {
+          if (a + d >= H || a - d < 0 || b + d >= W || b - d < 0) {
+            ok = false;
+            break;
+          }
+
+          if (C[a + d][b + d] == '.' || C[a + d][b - d] == '.'
+                || C[a - d][b + d] == '.' || C[a - d][b - d] == '.') ok = false;
+        }
+
+        if (ok) ans++;
+      }
+    }
+
+    cout << ans << ' ';
+  }
+  cout << endl;
+}
---
+++ atcoder/abc/251-300/300/d.cpp
@@ -0,0 +1,141 @@
+// #define _GLIBCXX_DEBUG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = numeric_limits<int>::max() / 2;
+const ll infl = numeric_limits<ll>::max()  / 2;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vl  = vector<ll>;
+using vvl = vector<vl>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvb = vector<vb>;
+using pii = pair<int, int>;
+
+// <----- rep macro ----->
+#define rep(var, a, b)  for (auto var = (a); var < (b); var++)
+#define rrep(var, a, b) for (auto var = a - 1; var >= (b); var--)
+#define fore(e, x) for (auto &&e: x)
+#define fore2(k, v, x) for (auto &[k, v]: x)
+
+// <----- other macro ----->
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <typename T>
+bool chmax(T &a, T b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <typename T>
+bool chmin(T &a, T b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+// <----- ModInt ----->
+template<int MOD> struct modint {
+  ll val;
+  constexpr modint(ll v = 0) noexcept : val(v % MOD) { if (val < 0) val += MOD; }
+  constexpr int getmod() { return MOD; }
+  constexpr modint operator - () const noexcept { return val ? MOD - val : 0; }
+  constexpr modint operator + (const modint& r) const noexcept { return modint(*this) += r; }
+  constexpr modint operator - (const modint& r) const noexcept { return modint(*this) -= r; }
+  constexpr modint operator * (const modint& r) const noexcept { return modint(*this) *= r; }
+  constexpr modint operator / (const modint& r) const noexcept { return modint(*this) /= r; }
+  constexpr modint& operator += (const modint& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr modint& operator -= (const modint& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr modint& operator *= (const modint& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr modint& operator /= (const modint& r) noexcept {
+    ll a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const modint& r) const noexcept { return this->val == r.val; }
+  constexpr bool operator != (const modint& r) const noexcept { return this->val != r.val; }
+  friend constexpr ostream& operator << (ostream &os, const modint<MOD>& x) noexcept { return os << x.val; }
+  constexpr modint<MOD> modpow(ll n) noexcept {
+      if (n == 0) return 1;
+      auto t = modpow(n / 2);
+      t *= t;
+      if (n & 1) t *= modint(*this);
+      return t;
+  }
+};
+
+const int MOD = 1000000007;
+using mint = modint<MOD>;
+
+
+// 1 以上 N 以下の整数が素数かどうかを返す
+vb Eratosthenes(int n) {
+  vb isprime(n + 1, true);
+  isprime[0] = false;
+  isprime[1] = false;
+
+  rep(p, 2, n + 1) {
+    // すでに合成数であるものはスキップ
+    if (!isprime[p]) continue;
+
+    // p 以外の p の倍数から素数ラベルを剥奪
+    for (int q = p * 2; q <= n; q += p) isprime[q] = false;
+  }
+
+  return isprime;
+}
+
+int main() {
+  ll N; cin >> N;
+
+  int max_p = min(N, 290000LL);
+  vb is_prime = Eratosthenes(max_p);
+  vl prime;
+  rep(i, 0, max_p) {
+    if (is_prime[i]) prime.push_back(i);
+  }
+
+  int ans = 0;
+  rep(i, 0, SIZE(prime)) rep(j, i + 1, SIZE(prime)) rep(k, j + 1, SIZE(prime)) {
+    ll temp = prime[i] * prime[i] * prime[j];
+    if (temp > N) break;
+
+    temp *= prime[k];
+    if (temp > N) break;
+
+    temp *= prime[k];
+    if (temp > N) break;
+
+    ans++;
+  }
+
+  cout << ans << endl;
+}
