c5ca0be4b2f4cef9842aee0f2a46fdc09e264d46
yutachaan <yutachan010907@gmail.com>
2024-01-08 17:19:27 +0200

    add abc304 a-e

---
+++ atcoder/abc/301-350/304/e.cpp
@@ -0,0 +1,153 @@
+// #define _GLIBCXX_DEBUG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = numeric_limits<int>::max() / 2;
+const ll infl = numeric_limits<ll>::max()  / 2;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vl  = vector<ll>;
+using vvl = vector<vl>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvb = vector<vb>;
+using pii = pair<int, int>;
+
+// <----- rep macro ----->
+#define rep(var, a, b)  for (auto var = (a); var < (b); var++)
+#define rrep(var, a, b) for (auto var = a - 1; var >= (b); var--)
+#define fore(e, x) for (auto &&e: x)
+#define fore2(k, v, x) for (auto &[k, v]: x)
+
+// <----- other macro ----->
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <typename T>
+bool chmax(T &a, T b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <typename T>
+bool chmin(T &a, T b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+// <----- ModInt ----->
+template<int MOD> struct modint {
+  ll val;
+  constexpr modint(ll v = 0) noexcept : val(v % MOD) { if (val < 0) val += MOD; }
+  constexpr int getmod() { return MOD; }
+  constexpr modint operator - () const noexcept { return val ? MOD - val : 0; }
+  constexpr modint operator + (const modint& r) const noexcept { return modint(*this) += r; }
+  constexpr modint operator - (const modint& r) const noexcept { return modint(*this) -= r; }
+  constexpr modint operator * (const modint& r) const noexcept { return modint(*this) *= r; }
+  constexpr modint operator / (const modint& r) const noexcept { return modint(*this) /= r; }
+  constexpr modint& operator += (const modint& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr modint& operator -= (const modint& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr modint& operator *= (const modint& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr modint& operator /= (const modint& r) noexcept {
+    ll a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const modint& r) const noexcept { return this->val == r.val; }
+  constexpr bool operator != (const modint& r) const noexcept { return this->val != r.val; }
+  friend constexpr ostream& operator << (ostream &os, const modint<MOD>& x) noexcept { return os << x.val; }
+  constexpr modint<MOD> modpow(ll n) noexcept {
+      if (n == 0) return 1;
+      auto t = modpow(n / 2);
+      t *= t;
+      if (n & 1) t *= modint(*this);
+      return t;
+  }
+};
+
+const int MOD = 1000000007;
+using mint = modint<MOD>;
+
+struct UnionFind {
+  vi par;
+
+  UnionFind(int n = 0): par(n, -1) {}
+
+  int root(int x) {
+    if (par[x] < 0) return x;
+    return par[x] = root(par[x]);
+  }
+
+  int size(int x) { return -par[root(x)]; }
+
+  bool same(int x, int y) { return root(x) == root(y); }
+
+  void unite(int x, int y) {
+    x = root(x); y = root(y);
+    if (x == y) return;
+    if (par[x] > par[y]) swap(x, y);
+    par[x] += par[y];
+    par[y] = x;
+  }
+};
+
+int main() {
+	int N, M; cin >> N >> M;
+  UnionFind uf(N);
+  rep(i, 0, M) {
+    int u, v; cin >> u >> v;
+    u--; v--;
+
+    uf.unite(u, v);
+  }
+
+  int K; cin >> K;
+  set<pii> NG_pair;
+  rep(i, 0, K) {
+    int x, y; cin >> x >> y;
+    x--; y--;
+
+    // つないではいけないrootをsetで管理
+    NG_pair.insert(make_pair(uf.root(x), uf.root(y)));
+  }
+
+  int Q; cin >> Q;
+  while (Q--) {
+    int p, q; cin >> p >> q;
+    p--; q--;
+
+    // 与えられたペアのルートがNG_pairだったらNo
+    bool ans = true;
+    if (NG_pair.count(make_pair(uf.root(p), uf.root(q)))) ans = false;
+    if (NG_pair.count(make_pair(uf.root(q), uf.root(p)))) ans = false;
+
+    YesNo(ans);
+  }
+}
