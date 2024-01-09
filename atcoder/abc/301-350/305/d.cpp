a5f74f65a75fcba76dfb2d933f33682846ebbd96
yutachaan <yutachan010907@gmail.com>
2024-01-09 13:50:05 +0200

    add abc 305 a-d

---
+++ atcoder/abc/301-350/305/d.cpp
@@ -0,0 +1,126 @@
+// #define _GLIBCXX_DEBUG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// <----- alias ----->
+using ll = long long;
+
+const int inf = numeric_limits<int>::max() / 2;
+const ll infl = numeric_limits<ll>::max()  / 2;
+
+using vi  = vector<int>;
+using vvi = vector<vi>;
+using vl  = vector<ll>;
+using vvl = vector<vl>;
+using vs  = vector<string>;
+using vb  = vector<bool>;
+using vvb = vector<vb>;
+using pii = pair<int, int>;
+
+// <----- rep macro ----->
+#define rep(var, a, b)  for (auto var = (a); var < (b); var++)
+#define rrep(var, a, b) for (auto var = a - 1; var >= (b); var--)
+#define fore(e, x) for (auto &&e: x)
+#define fore2(k, v, x) for (auto &[k, v]: x)
+
+// <----- other macro ----->
+#define ALL(x) begin(x), end(x)
+#define RALL(x) rbegin(x), rend(x)
+#define SIZE(x) ll((x).size())
+
+#define YESNO(n) cout << ((n) ? "YES" : "NO") << endl
+#define yesno(n) cout << ((n) ? "yes" : "no") << endl
+#define YesNo(n) cout << ((n) ? "Yes" : "No") << endl
+
+// <----- function ----->
+template <typename T>
+bool chmax(T &a, T b) {
+  if (a < b) {a = b; return true;}
+  return false;
+}
+template <typename T>
+bool chmin(T &a, T b) {
+  if (a > b) {a = b; return true;}
+  return false;
+}
+
+// <----- ModInt ----->
+template<int MOD> struct modint {
+  ll val;
+  constexpr modint(ll v = 0) noexcept : val(v % MOD) { if (val < 0) val += MOD; }
+  constexpr int getmod() { return MOD; }
+  constexpr modint operator - () const noexcept { return val ? MOD - val : 0; }
+  constexpr modint operator + (const modint& r) const noexcept { return modint(*this) += r; }
+  constexpr modint operator - (const modint& r) const noexcept { return modint(*this) -= r; }
+  constexpr modint operator * (const modint& r) const noexcept { return modint(*this) *= r; }
+  constexpr modint operator / (const modint& r) const noexcept { return modint(*this) /= r; }
+  constexpr modint& operator += (const modint& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr modint& operator -= (const modint& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr modint& operator *= (const modint& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr modint& operator /= (const modint& r) noexcept {
+    ll a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      ll t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const modint& r) const noexcept { return this->val == r.val; }
+  constexpr bool operator != (const modint& r) const noexcept { return this->val != r.val; }
+  friend constexpr ostream& operator << (ostream &os, const modint<MOD>& x) noexcept { return os << x.val; }
+  constexpr modint<MOD> modpow(ll n) noexcept {
+      if (n == 0) return 1;
+      auto t = modpow(n / 2);
+      t *= t;
+      if (n & 1) t *= modint(*this);
+      return t;
+  }
+};
+
+const int MOD = 1000000007;
+using mint = modint<MOD>;
+
+
+int main() {
+  int N; cin >> N;
+  vl A(N); rep(i, 0, N) cin >> A[i];
+
+  // 各iでの寝た時間の累計を算出
+  vl total_sleep(N, 0);
+  rep(i, 2, N) {
+    if (i % 2 == 0) total_sleep[i] = total_sleep[i - 1] + (A[i] - A[i - 1]);
+    else total_sleep[i] = total_sleep[i - 1];
+  }
+
+  int Q; cin >> Q;
+  while (Q--) {
+    ll l, r; cin >> l >> r;
+
+    // l, r以上のはじめてのA[i]のインデックスを取得
+    int l_i = lower_bound(ALL(A), l) - begin(A);
+    int r_i = lower_bound(ALL(A), r) - begin(A);
+
+    // l, rまでに寝た時間の累計を算出
+    ll l_total = l_i % 2 == 0 ? total_sleep[l_i] - (A[l_i] - l) : total_sleep[l_i];
+    ll r_total = r_i % 2 == 0 ? total_sleep[r_i] - (A[r_i] - r) : total_sleep[r_i];
+
+    ll ans = r_total - l_total;
+
+    cout << ans << endl;
+  }
+}
